1f5bb4ff0775e9c46825e562317f1eea88928035
mugman <53713683+nature174@users.noreply.github.com>
2021-08-04 08:42:50 -0700

    RPC Plugin

--- rpc/index.js
+++ rpc/index.js
@@ -1,6 +1,7 @@
 import { createItem, removeItem } from "@goosemod/settings";
 
 let stor = {};
+let togl = true;
 export default {
   goosemodHandlers: {
     onImport: () => {
@@ -15,11 +16,21 @@ export default {
                 }
               : undefined,
           assets: {
-            large_image: undefined,
-            small_image: undefined,
-            large_text: undefined || undefined,
-            small_text: undefined || undefined,
+            large_image: stor.large_image || undefined,
+            small_image: stor.small_image || undefined,
+            large_text: stor.large_text || undefined,
+            small_text: stor.small_text || undefined,
           },
+          buttons: [
+            {
+              label: stor.label_one || undefined,
+              url: stor.url_one || undefined,
+            },
+            {
+              label: stor.label_two || undefined,
+              url: stor.url_two || undefined,
+            },
+          ],
         };
       }
       goosemod.webpackModules
@@ -58,9 +69,45 @@ export default {
           });
       }
 
+      function removeStatus() {
+        goosemod.webpackModules
+          .findByProps("INVITE_BROWSER")
+          .SET_ACTIVITY.handler({
+            socket: {
+              id: 100,
+              application: {
+                id: stor.appid || "733854571738300487",
+                name: stor.name || "RPC",
+              },
+              transport: "ipc",
+            },
+            args: {
+              pid: 10,
+              activity: undefined,
+            },
+          });
+      }
+
       createItem("RPC", [
         "(1.0.0)",
         // field objects
+        {
+          type: "toggle",
+          text: "Enable RPC",
+          onToggle: (value) => {
+            if (value) {
+              setStatus();
+              togl = ! togl
+            } else {
+              removeStatus();
+              togl = ! togl
+            }
+          },
+          isToggled: () => {
+            // Define what state the toggle switch should be in when loading the page.
+            return togl;
+          },
+        },
         {
           type: "custom",
           element: (() => {
@@ -136,6 +183,155 @@ export default {
           })(),
         },
         {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.large_image = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Large Image Name",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.large_text = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Large Image Text",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.small_image = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Small Image Name",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.small_text = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Small Image Text",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.label_one = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Button One Text",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.url_one = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Button One URL",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.label_two = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Button Two Text",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+
+        {
+          type: "custom",
+          element: (() => {
+            const elm = goosemod.webpackModules.common.React.createElement(
+              goosemod.webpackModules.findByDisplayName("TextInput"),
+              {
+                onBlur: (val) => {
+                  stor.url_two = val.target.value || undefined;
+                  setStatus();
+                },
+                placeholder: "Button Two URL",
+              }
+            );
+            let e = document.createElement("div");
+            goosemod.webpackModules.common.ReactDOM.render(elm, e);
+            return e;
+          })(),
+        },
+
+
+        {
           type: "toggle",
           text: "Show timestamp",
           onToggle: (value) => {
@@ -153,9 +349,28 @@ export default {
     onRemove: () => {
       try {
         removeItem("RPC");
+        function removeStatus() {
+          goosemod.webpackModules
+            .findByProps("INVITE_BROWSER")
+            .SET_ACTIVITY.handler({
+              socket: {
+                id: 100,
+                application: {
+                  id: stor.appid || "733854571738300487",
+                  name: stor.name || "RPC",
+                },
+                transport: "ipc",
+              },
+              args: {
+                pid: 10,
+                activity: undefined,
+              },
+            });
+        }
+        removeStatus();
       } catch {
         console.warn(
-          `Run goosemod.settings.removeItem("RPC") if the settings page does not disappear.`
+          `Run goosemod.settings.removeItem("RPC") if the settings page does not disappear. RPC may not disappear.`
         );
       }
     },
